642c34daf0ebc1869f59f14cb85ef47779f7f74d
Hoai Viet Nguyen <hvtnguyen@yahoo.de>
2012-06-11 16:30:15 +0200

    d

--- websocket-server.js
+++ websocket-server.js
@@ -24,7 +24,7 @@ function originIsAllowed(origin) {
   // put logic here to detect whether the specified origin is allowed.
   return true;
 }
-
+var connections = 0;
 wsServer.on('request', function(request) {
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
@@ -34,18 +34,21 @@ wsServer.on('request', function(request) {
     }
 
     var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
+		connections++;
+		console.log(connections);
+    //console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
+            //console.log('Received Message: ' + message.utf8Data);
             connection.sendUTF(message.utf8Data);
         }
         else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            //console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
             connection.sendBytes(message.binaryData);
         }
     });
     connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+				console.log(connections);
+        //console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
